c2c050274aeba37f0776911c382612fb9ebfbf1c
Marwan Hawari <marwan.hawari@gmail.com>
2021-12-19 10:20:40 -0800

    working

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
+import os
+import sys
 import requests
 from pathlib import Path
 from ghget.gh import GH
-import os
 
 gh = GH(
-    "https://github.com/marwanhawari/pyinterview/tree/main/docs/algorithms",
+    "https://github.com/marwanhawari/pyinterview/tree/main/docs/",
     os.getenv("GITHUB_TOKEN"),
 )
 
@@ -24,45 +25,32 @@ def generate_raw_url(owner, repo, branch, file_path):
 
 def download_file(raw_file_url: str, file_name: str) -> None:
     response = requests.get(raw_file_url)
-
     file_content = response.content
-
     with open(file_name, "wb") as f:
         f.write(file_content)
 
 
-for path in all_paths:
-    tree_path = Path(path["path"])
-    if tree_path.is_relative_to(gh.file_path):
-        if is_root:
-            print(str(tree_path), path["type"])
-        else:
-            partition_path = Path(str(tree_path).partition(f"{prefix_path}/")[-1])
-            if path["type"] == "tree":
-                # print(partition_path)
-                partition_path.mkdir(exist_ok=True, parents=True)
+def download_contents(path, gh, partition_path):
+    if path["type"] == "tree":
+        partition_path.mkdir(parents=True)
+    else:
+        raw_file_url = generate_raw_url(gh.owner, gh.repo, gh.branch, path["path"])
+        download_file(raw_file_url, partition_path)
+
+
+def main() -> int:
+    for path in all_paths:
+        tree_path = Path(path["path"])
+        if tree_path.is_relative_to(gh.file_path):
+            if is_root:
+                partition_path = tree_path
+                download_contents(path, gh, partition_path)
             else:
-                raw_file_url = generate_raw_url(
-                    gh.owner, gh.repo, gh.branch, path["path"]
-                )
-                # print(partition_path)
-                download_file(raw_file_url, partition_path)
+                partition_path = Path(str(tree_path).partition(f"{prefix_path}/")[-1])
+                download_contents(path, gh, partition_path)
+
+    return 0
 
 
-def download_dir(
-    http_response: requests.models.Response, file_path: str, headers: dict
-) -> None:
-    response_content = http_response.json()
-
-    Path(file_path).mkdir(exist_ok=True, parents=True)
-
-    for obj in response_content:
-
-        current_path = f'{file_path}/{obj["name"]}'
-
-        if obj["type"] == "file":
-            download_file(obj["download_url"], current_path)
-
-        elif obj["type"] == "dir":
-            response = requests.get(obj["url"], headers=headers)
-            download_dir(response, current_path, headers)
+if __name__ == "__main__":
+    sys.exit(main())
